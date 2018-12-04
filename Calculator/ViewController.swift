629da34e29c1cefd1d0dc402550d9b3763619503
Corentin <corentin.terente@gmail.com>
2018-12-04 13:45:03 +0100

    initialisation du projet calculette

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,95 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorOnResultLabel: UILabel!
+    @IBOutlet weak var dotButton: UIButton!
+    
+    var previousResult: Float = 0
+    var previousOperation : String?
+    var isResultDisplayed: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func onDigitTaped(_ sender: UIButton) {
+        
+        guard let stringDigit = sender.titleLabel?.text else{
+            return
+        }
+        
+        if calculatorOnResultLabel.text == "0" || isResultDisplayed{
+            calculatorOnResultLabel.text = stringDigit
+            isResultDisplayed = false
+        } else {
+            calculatorOnResultLabel.text?.append(stringDigit)
+        }
+        
+        if stringDigit == "." {
+            dotButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func onOperationTapped(_ sender: UIButton) {
+        
+        guard let stringDigit = sender.titleLabel?.text else{
+            return
+        }
+        
+        guard let stringResultLabel = calculatorOnResultLabel.text else{
+            return
+        }
+        previousOperation = stringDigit
+        previousResult = Float(stringResultLabel) ?? 0
+        calculatorOnResultLabel.text = "0"
+        dotButton.isEnabled = true
+    }
+    
+    @IBAction func onEqualsTapped(_ sender: UIButton) {
+        
+        guard let operation = previousOperation else{
+            return
+        }
+        
+        guard let stringResultLabel = calculatorOnResultLabel.text else{
+            return
+        }
+        
+        let currentResult: Float? = Float(stringResultLabel)
+        
+        var result: Float = 0
+        
+        if operation == "+" {
+            
+            result = previousResult + (currentResult ?? 0)
+            calculatorOnResultLabel.text = String(result)
+            
+        } else if operation == "-" {
+            
+            result = previousResult - (currentResult ?? 0)
+            calculatorOnResultLabel.text = String(result)
+            
+        } else if operation == "*" {
+            
+            result = previousResult * (currentResult ?? 0)
+            calculatorOnResultLabel.text = String(result)
+            
+        } else if operation == "/" {
+            
+            if currentResult != 0 {
+                
+                result = previousResult / (currentResult ?? 0)
+                calculatorOnResultLabel.text = String(result)
+                
+            } else {
+                 calculatorOnResultLabel.text = "ERREUR"
+            }
+        }
+       
+        isResultDisplayed = true
+    }
+    
 }
 
